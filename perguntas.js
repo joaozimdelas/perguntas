2e660e9c53ea14ce0834f3a7ef13dc3193e79cef
joaozimdelas <143225014+joaozimdelas@users.noreply.github.com>
2024-10-18 09:31:00 -0300

    ,yuk,

---
+++ perguntas.js
@@ -0,0 +1,103 @@
+CriaCartao(
+"Ciências",
+"Qual é o gás mais abundante na atmosfera terrestre?",
+"Resposta: Nitrogênio"
+
+)
+CriaCartao(
+ "ciências",  
+  "O que os pulmões fazem durante a respiração?",
+    "Resposta: Trocam oxigênio e dióxido de carbono no sangue"
+)
+
+CriaCartao (
+    "historia",
+    "Quem foi o líder da Revolução Francesa?",
+    "Resposta: Maximilien Robespierre")
+
+CriaCartao (
+"Historia",
+    "Em que ano terminou a Segunda Guerra Mundial?",
+    "Resposta: 1945"
+)
+CriaCartao (
+   "geografia",
+    "qual o menor continente em área?",
+    "Resposta: Oceania"
+)
+CriaCartao(
+"Geogradia",
+    "Qual é o nome da cordilheira que atravessa a América do Sul?",
+"Resposta: Cordilheira dos Andes"
+)
+CriaCartao (
+" Português",
+"O que é um substantivo?",
+"Resposta: Palavra que nomeia seres, coisas, lugares, ideias etc."
+)
+CriaCartao ( 
+"portugues",
+"Qual é o antônimo de feliz?",
+"Resposta: Triste"
+)
+CriaCartao (
+
+    "portugues",
+    "Qual é o antônimo de feliz?",
+    "Resposta: Triste"
+)
+CriaCartao (
+   "Educação Física",
+    "Qual é o nome do movimento olímpico de saltar sobre uma barra alta usando uma vara?",
+    "Resposta: Salto com vara",
+)
+CriaCartao ( 
+    "Educação Física",
+    "Qual é o esporte que combina natação, ciclismo e corrida?",
+    "Resposta: Triatlo"
+)
+CriaCartao(
+    "Artes",
+    "Quem pintou o teto da Capela Sistina?",
+    "Resposta: Michelangelo"
+)
+CriaCartao(
+
+    "Arte",
+    "O que significa “Renaissance” em termos de história da arte?",
+"Resposta: Renascimento, período de redescoberta da arte clássica"
+)
+CriaCartao (
+    "Inglês",
+    "Como se diz amigo em inglês?",
+    "Resposta: Friend"
+)
+CriaCartao(
+
+    "Ingles" ,
+    "Qual é o passado do verbo go em inglês?",
+"Resposta: Went"
+)
+CriaCartao (
+
+"Física",
+"O que é a gravidade?",
+"Resposta: Força que atrai os objetos para o centro da Terra ou outros corpos celestes"
+)
+CriaCartao ( 
+    "Física",
+
+    "Qual é a unidade de medida da energia?",
+    "Resposta: Joule"  
+)
+
+CriaCartao(
+    "Química",
+    "Qual é o símbolo químico do ferro?",
+    "Resposta: Fe"
+)
+CriaCartao( 
+"Química",
+"Qual é o pH de uma substância neutra?",
+"Resposta: 7"
+)
